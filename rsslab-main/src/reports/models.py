07e2a757c23b113ec9a29e769aec209b7d89d230
haseeb-admani <60135404+haseeb-admani@users.noreply.github.com>
2022-08-15 05:00:41 +0500

    DBStuff

--- rsslab-main/src/reports/models.py
+++ rsslab-main/src/reports/models.py
@@ -46,7 +46,8 @@ def create_entries(sender, instance, **kwargs):
     # print(data.index)
     today = date.today()
     conn = db.connect('db.sqlite3')
-    data.to_sql(name='reports_patientnotes', con=conn, if_exists="append", index = True, index_label = "id")
+    data.to_sql(name='reports_patientnotes', con=conn, if_exists="append", index = False)
+    conn.commit()
     # for row in data.itertuples():
     #     # print(row)
     #     # print(row.case_num)
